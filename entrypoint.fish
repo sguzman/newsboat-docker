f58e534fa20589ab17611f7d47352380f1c30808
Salvador Guzman <guzmansalv@gmail.com>
2024-05-21 08:11:34 -0700

    fish

--- entrypoint.fish
+++ entrypoint.fish
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/usr/bin/fish
 
 # Run Newsboat reload in an infinite loop with a sleep interval
-while true; do
+while true
     newsboat --log-level 5 --log-file /dev/stdout -x reload
     pgloader /root/.newsboat/cache.db postgresql://admin:admin@postgres:5555/rss
     sleep 300
-done
+end
 
